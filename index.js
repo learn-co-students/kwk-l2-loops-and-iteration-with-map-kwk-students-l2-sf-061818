61df0bd1c757c43e670718a60c168cbe6786ca82
axaud <aarmientaa@gmail.com>
2018-06-21 20:56:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file.
+// Code your solution in this file
+let studentNames = []
+function lowerCaseStudentNames(names) {
+  for (var counter = 0; counter < names.length; counter++) {
+  var name = names[counter];
+  studentNames.push(name.toLowerCase())
+  }
+  return studentNames
+}
+
+let separation = []
+var fullNames = ['Amy Adams', 'Cate Blanchett', 'Emma Stone', 'Jennifer Lawrence', 'Natalie Portman']
+function splitFunction(students) {
+  students = students.split(" ")
+  students = {
+    firstName:students[0],
+    lastName:students[1]
+  }
+  separation.push(students)
+}
+function nameToAttributes(name) {
+  name.map(splitFunction)
+  return separation
+}
+
+let from = [];
+function sentenceMaker(object){
+   from.push(`${object.name} is from ${object.hometown}`);
+}
+function attributesToPhrase(array){
+  array.map(sentenceMaker);
+  return from;
+}
+
