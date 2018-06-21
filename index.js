0787fdba0218afac4ab6a456097df8cc18c2c193
Areeta Wong <areetaw@gmail.com>
2018-06-21 18:39:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file.
+
+const lowerArray = [];
+
+function lower(arr) {
+  arr = arr.toLowerCase();
+  lowerArray.push(arr);
+}
+
+function lowerCaseStudentNames(students) {
+  students.map(lower);
+  return lowerArray;
+}
+
+const splitArray = [];
+
+function nameToAttributes(fullNames) {
+  fullNames.map(split);
+  return splitArray;
+}
+
+function split(full) {
+    full = full.split(" ");
+    var name = {
+      firstName: full[0],
+      lastName: full[1],
+    }
+    splitArray.push(name)
+}
+
+const attArray = [];
+
+function attributesToPhrase(objects) {
+  objects.map(attributes);
+  return attArray;
+}
+
+function attributes(nameAndPlace) {
+  var name = nameAndPlace.name;
+  var hometown = nameAndPlace.hometown;
+  var str = name + " is from " + hometown;
+  attArray.push(str);
+}
+
+
+
